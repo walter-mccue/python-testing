fb9f9ab79ab109c0060ff5cab6d48b3a695bb185
Walter McCue <walter.mccue@gmail.com>
2024-01-07 11:51:23 -0500

    tkinter testing

---
+++ tk.py
@@ -0,0 +1,25 @@
+#Import the required libraries
+from tkinter import *
+
+#Create an instance of tkinter frame
+win= Tk()
+
+#Define the size of window or frame
+win.geometry("800x500")
+
+win.title("BATTLESHIP GAME")
+
+#Set the Menu initially
+menu= StringVar()
+menu.set("Do you want to play Battleship?")
+
+#Create a dropdown Menu
+drop = OptionMenu(win, menu,"Yes", "No")
+drop.pack()
+
+if menu == "Yes":
+  print ("Let's Play!")
+
+win.mainloop()
+
+
